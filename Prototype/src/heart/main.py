f49560076176894bbf4fd7af756932d19e21d696
nicola <niko.ramak@gmail.com>
2025-06-06 17:49:06 +0200

    heart rate class modular implemenation based on interrupt handler rather than polling

---
+++ Prototype/src/heart/main.py
@@ -0,0 +1,15 @@
+import time as Time
+from heart_rate_sensor import HeartRateSensor
+
+if __name__ == "__main__":
+    heart_rate_sensor = HeartRateSensor(gpio_pin_hr=4, gpio_pin_led=17)
+    heart_rate_sensor.setup()
+
+    try:
+        print("Sistema in ascolto (interrupt attivo)...")
+        while True:
+            Time.sleep(1)  # Il thread principale dorme
+    except KeyboardInterrupt:
+        print("\nTerminazione da tastiera...")
+    finally:
+        heart_rate_sensor.cleanup()
